c6454fba5c8a11f57f6b8a2c06d7a1fa8053e7e3
blinky <martin@foam.space>
2022-01-06 19:47:57 +0000

    iterators

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -3,6 +3,7 @@
 // I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
+    (1..(num + 1)).fold(1,|x,acc| x * acc)
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
